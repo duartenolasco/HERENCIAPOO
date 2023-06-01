d47ff2e7f591fc70a5e94de8cb99dc9697f88682
duartenolasco <lduarten@miumg.edu.gt>
2023-05-31 23:55:08 -0600

    uso de la herencia en la calculadora normal a la avanzada

---
+++ calcularaespecial.java
@@ -0,0 +1,13 @@
+
+package tareaprograiipoo;
+
+public class calcularaespecial {
+    
+      public double multiplicar(double primerNumero, double segundoNumero){
+        return primerNumero * segundoNumero;
+    }
+    
+    public double dividir(double primerNumero, double segundoNumero){
+        return primerNumero / segundoNumero;
+    }
+}
